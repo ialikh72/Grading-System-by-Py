647d807cdb6b756ee328ad1cd69d6605933c98ce
ialikh72 <ialikh.w1@gmail.com>
2026-03-14 22:55:09 +0500

    Add project files.

---
+++ Grading_System_by_Py.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter your marks: "))
+if(marks>=90):
+    print("Your GPA is 4.0")
+elif(marks>=80):
+    print("Your GPA is 3.0")
+elif(marks>=70):
+    print("Your GPA is 2.0")
+elif(marks>=60):
+    print("Your GPA is 1.0")
+else:
+    print("Your GPA is 0.0")
